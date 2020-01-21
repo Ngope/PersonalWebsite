9b227348c367feb7c4230371b81307648c7e6915
Peterson Ngo <ngop0629@gmail.com>
2020-01-20 21:00:36 -0500

    work work owrk work

--- src/components/aboutme.vue
+++ src/components/aboutme.vue
@@ -2,6 +2,7 @@
   <div>
   <b-jumbotron header="Aspiring Software Engineer" lead="Indiana University Bloomington Class of 2022">
     <h1> Peterson Ngo.</h1>
+    <b-button variant="dark">Download Resume</b-button>
   </b-jumbotron>
     <b-container fluid>
       <b-row>
@@ -55,9 +56,63 @@
             </tr>
             </tbody>
           </table>
+          <b-jumbotron lead="About me:">
+            <br>
+             <p>
+               I am currently attending Indiana University Bloomington as a Computer Science major with a minor in Informatics.
+               This project not only showcases my skills as a programmer, but it was intended to be a fun project to show my resume.
+               I plan on updating this website as I learn more and more about design and as I become a better programmer.
+             </p>
+          </b-jumbotron>
         </b-col>
       </b-row>
     </b-container>
+    <hr>
+    <div class="cards">
+    <b-alert show variant="light">Jobs Timeline</b-alert>
+      <div class="mt-3">
+        <b-card-group deck>
+          <b-card bg-variant="light" header="Goshen Noodle(Server) May 2016 - May 2019">
+            <b-card-text>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</b-card-text>
+          </b-card>
+
+          <b-card bg-variant="dark" header="Jimmy John's (Deliver Driver/Team Member) May 2019 - August 2019 " text-variant="white" class="text-center">
+            <b-card-text>Worked in the Kitchen to prepare sandwiches and talked to incoming customers as they ordered the sandwiches. If I was not in the store,
+              I was delivering sandwiches that were phone ordered or ordered online. As a team member, I helped clean and talked to many customers.
+              Working with many other employees showed me a sense of teamwork in order to achieve a common goal.</b-card-text>
+          </b-card>
+
+          <b-card bg-variant="light" header="IU Safety Escort (Navigation) August 2019 - Present" class="text-center">
+            <b-card-text>As a member of IU safety escort, my job is to help the driver navigate through on and off campus locations to drop off students who are using our service.
+              The skills required for this job include multi-tasking and being able to well with others.
+              Working as a team leads to swift and smooth rides. </b-card-text>
+          </b-card>
+        </b-card-group>
+        <hr>
+        <b-alert show variant="light">Clubs</b-alert>
+        <div class="mt-3">
+          <b-card-group deck>
+            <b-card bg-variant="dark" header="Jimmy John's (Deliver Driver/Team Member) May 2019 - August 2019 " text-variant="white" class="text-center">
+              <b-card-text>Worked in the Kitchen to prepare sandwiches and talked to incoming customers as they ordered the sandwiches. If I was not in the store,
+                I was delivering sandwiches that were phone ordered or ordered online. As a team member, I helped clean and talked to many customers.
+                Working with many other employees showed me a sense of teamwork in order to achieve a common goal.</b-card-text>
+            </b-card>
+
+            <b-card bg-variant="light" header="IU Safety Escort (Navigation) August 2019 - Present" class="text-center">
+              <b-card-text>As a member of IU safety escort, my job is to help the driver navigate through on and off campus locations to drop off students who are using our service.
+                The skills required for this job include multi-tasking and being able to well with others.
+                Working as a team leads to swift and smooth rides. </b-card-text>
+            </b-card>
+
+            <b-card bg-variant="dark" header="Jimmy John's (Deliver Driver/Team Member) May 2019 - August 2019 " text-variant="white" class="text-center">
+              <b-card-text>Worked in the Kitchen to prepare sandwiches and talked to incoming customers as they ordered the sandwiches. If I was not in the store,
+                I was delivering sandwiches that were phone ordered or ordered online. As a team member, I helped clean and talked to many customers.
+                Working with many other employees showed me a sense of teamwork in order to achieve a common goal.</b-card-text>
+            </b-card>
+          </b-card-group>
+        </div>
+      </div>
+      </div><!---ends the card div-->
   </div>
 </template>
 
@@ -71,5 +126,8 @@ export default {
   img {
     border-radius: 50%;
     padding-bottom: 60px;
+  }
+  cards{
+    padding: 10px;
   }
 </style>
